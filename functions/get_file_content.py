315b906ff2f4000afdbec9f16e2fdf6d4b872759
Iniyaa21 <iniyaapaari@gmail.com>
2025-09-09 18:15:26 +0530

    Add config.py

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,5 +1,5 @@
 import os
-
+import config
 
 def get_file_content(working_directory, file_path):
     abs_working_directory = os.path.abspath(working_directory)
@@ -9,7 +9,7 @@ def get_file_content(working_directory, file_path):
     if not os.path.isfile(abs_file_path): 
         return f'Error: File not found or is not a regular file: "{file_path}"'
     
-    MAX_CHARS = 10000 
+    MAX_CHARS = config.MAX_CHARS 
     
     with open(abs_file_path, "r") as f: 
         file_content_string = f.read(MAX_CHARS) 
